2289455b146b34f652d3c01073a0b3250b374326
osada-yum <osada.yum@gmail.com>
2024-02-07 19:55:20 +0900

    files for 2D-XY model.

---
+++ app/xy2d_relaxation.f90
@@ -0,0 +1,5 @@
+program xy2d_relaxation
+  use xy2d_m
+  implicit none
+
+end program xy2d_relaxation
---
+++ src/xy2d_m.f90
@@ -0,0 +1,12 @@
+module xy2d_m
+  use, intrinsic :: iso_fortran_env
+  implicit none
+  private
+  public :: xy2d
+  type :: xy2d
+     private
+
+   contains
+  end type xy2d
+contains
+end module xy2d_m
